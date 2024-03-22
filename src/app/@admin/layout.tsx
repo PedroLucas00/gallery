c849848e8878b9a02980602993565c8ff3092c2b
pedroLucas00 <pedrowalmart10@gmail.com>
2024-03-22 09:57:49 -0300

    Fix bugs

--- src/app/@admin/layout.tsx
+++ src/app/@admin/layout.tsx
@@ -2,7 +2,7 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
 import { useSelectedLayoutSegment } from 'next/navigation'
 import TopMenu from "@/components/top-menu";
 import FooterComponent from "@/components/footer";
